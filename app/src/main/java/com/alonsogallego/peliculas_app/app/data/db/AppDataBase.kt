fa162d99d9bf00bc63a27508bd5804bcd5a924f3
alonsogallego7 <alonso.galpac@educa.jcyl.es>
2022-11-11 23:51:10 +0100

    tested xml, mem and db

--- app/src/main/java/com/alonsogallego/peliculas_app/app/data/db/AppDataBase.kt
+++ app/src/main/java/com/alonsogallego/peliculas_app/app/data/db/AppDataBase.kt
@@ -3,9 +3,9 @@ package com.alonsogallego.peliculas_app.app.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.alonsogallego.peliculas_app.peliculas.data.pelicula.local.db.PeliculaDao
-import com.alonsogallego.peliculas_app.peliculas.domain.models.Pelicula
+import com.alonsogallego.peliculas_app.peliculas.data.pelicula.local.db.PeliculaEntity
 
-@Database(entities = [Pelicula::class], version = 1)
+@Database(entities = [PeliculaEntity::class], version = 1)
 abstract class AppDataBase : RoomDatabase() {
     abstract fun peliculaDao(): PeliculaDao
 }
